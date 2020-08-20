838fd23a4b209796e379ee51f1df3685e77317e1
Cesar Munoz <cesar.alberto.munoz@gmail.com>
2020-08-20 10:14:12 -0300

    feat: adding tailwind configuration

--- src/App.js
+++ src/App.js
@@ -49,6 +49,16 @@ function App() {
           >
             React Redux
           </a>
+          <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+            <div className="ml-6 pt-1">
+              <h1 className="text-2xl text-blue-700 leading-tight">
+                Tailwind and Create React App
+              </h1>
+              <p className="text-base text-gray-700 leading-normal">
+                Building apps together
+              </p>x
+            </div>
+          </div>
         </span>
       </header>
     </div>
